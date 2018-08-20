14353de73a5d5b083dbaa304377b0b17af159410
biteSMS <348856022@qq.com>
2018-08-20 19:47:46 +0800

    完成军训视频

---
+++ src/components/training/Video1.vue
@@ -0,0 +1,23 @@
+<template>
+    <tr-video :typepath="video1" class="player"></tr-video>
+</template>
+
+<script>
+import TrVideo from "../common/Tvideo"
+
+export default {
+    components: {
+        TrVideo
+    },
+    data() {
+        return {
+            video1: [
+                {
+                    type: "video/mp4",
+                    src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm",
+                }
+            ]
+        }
+    }
+}
+</script>

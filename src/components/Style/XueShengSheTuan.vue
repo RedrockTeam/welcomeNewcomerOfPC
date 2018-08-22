8c57c1eb5ff77ed0069cc6785f3fc072f23fc3a5
biteSMS <348856022@qq.com>
2018-08-22 21:27:51 +0800

    完成学生社团

--- src/components/Style/XueShengSheTuan.vue
+++ src/components/Style/XueShengSheTuan.vue
@@ -1,6 +1,231 @@
 <template>
     <div class="xueshengshetuan">
-
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/stongxin.png"></div>
+            <div class="container">
+                <div class="content">畅响室内乐社</div>
+                <div class="content">电子协会</div>
+                <div class="content">爱心社</div>
+                <div class="content">科技论文讲习社</div>
+                <div class="content">曲艺社百闻</div>
+                <div class="content">茶道社</div>
+                <div class="content">CF对外交流协会</div>
+                <div class="content">移动互联网创新中心</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sjisuanji.png"></div>
+            <div class="container">
+                <div class="content">信息安全协会</div>
+                <div class="content">电子竞技协会</div>
+                <div class="content">数码协会</div>
+                <div class="content">软件技术与应用协会</div>
+                <div class="content">硬件技术与应用协会</div>
+                <div class="content">Linux协会</div>
+                <div class="content">街舞协会</div>
+                <div class="content">软通动力英才俱乐部</div>
+                <div class="content">ACM程序设计协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sxianjin.png"></div>
+            <div class="container">
+                <div class="content">机器人协会</div>
+                <div class="content">电子协会</div>
+                <div class="content">爱心社</div>
+                <div class="content">科技论文讲习社</div>
+                <div class="content">曲艺社</div>
+                <div class="content">百闻茶道社</div>
+                <div class="content">CF对外交流协会</div>
+                <div class="content">移动互联网创新中心</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/szidonghua.png"></div>
+            <div class="container">
+                <div class="content">推理协会</div>
+                <div class="content">创客部落粤语社</div>
+                <div class="content">象棋协会</div>
+                <div class="content">物联网协会</div>
+                <div class="content">桌游协会航模协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sxianjin.png"></div>
+            <div class="container">
+                <div class="content">机器人协会</div>
+                <div class="content">电子协会</div>
+                <div class="content">爱心社</div>
+                <div class="content">科技论文讲习社</div>
+                <div class="content">曲艺社</div>
+                <div class="content">百闻茶道社</div>
+                <div class="content">CF对外交流协会</div>
+                <div class="content">移动互联网创新中心</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/szidonghua.png"></div>
+            <div class="container">
+                <div class="content">推理协会</div>
+                <div class="content">创客部落粤语社</div>
+                <div class="content">象棋协会</div>
+                <div class="content">物联网协会</div>
+                <div class="content">桌游协会航模协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sguangdian.png"></div>
+            <div class="container">
+                <div class="content">科幻协会</div>
+                <div class="content">半导体协会</div>
+                <div class="content">TG精英社</div>
+                <div class="content">科技创新协会</div>
+                <div class="content">道路交通安全协会</div>
+                <div class="content">素质拓展协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sruanjian.png"></div>
+            <div class="container">
+                <div class="content">龙影棍道协会</div>
+                <div class="content">跑步爱好者协会</div>
+                <div class="content">西乐社CSDN俱乐部</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sshengwu.png"></div>
+            <div class="container">
+                <div class="content">天元围棋协会</div>
+                <div class="content">魔方协会基因社</div>
+                <div class="content">生命科学协会</div>
+                <div class="content">绿溢南山环保协会</div>
+                <div class="content">动物保护协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sli.png"></div>
+            <div class="container">
+                <div class="content">天文爱好者协会</div>
+                <div class="content">数学俱乐部</div>
+                <div class="content">数学建模协会</div>
+                <div class="content">物理协会</div>
+                <div class="content">科普写作社</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sjingji.png"></div>
+            <div class="container">
+                <div class="content">电子商务协会</div>
+                <div class="content">金融协会</div>
+                <div class="content">创业者协会</div>
+                <div class="content">市场营销协会</div>
+                <div class="content">ERP协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/schuangmei.png"></div>
+            <div class="container">
+                <div class="content">CQUPT动漫协会</div>
+                <div class="content">电影协会</div>
+                <div class="content">摄影协会</div>
+                <div class="content">镝鹤汉服文化协会</div>
+                <div class="content">手作社民族乐器协会</div>
+                <div class="content">重邮剧社音乐协会</div>
+                <div class="content">南山文学社</div>
+                <div class="content">魔术协会</div>
+                <div class="content">美妆社</div>
+                <div class="content">花卉绿植社</div>
+                <div class="content">文峰画社</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sguoyu.png"></div>
+            <div class="container">
+                <div class="content">兰亭书社</div>
+                <div class="content">英语辩论协会</div>
+                <div class="content">翻译协会</div>
+                <div class="content">饮食文化协会</div>
+                <div class="content">英语俱乐部</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sguoyu.png"></div>
+            <div class="container">
+                <div class="content">Beat-box协会</div>
+                <div class="content">印迹3D打印社</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/swangluo.png"></div>
+            <div class="container">
+                <div class="content">红色社团</div>
+                <div class="content">射艺协会</div>
+                <div class="content">政法论坛诵读协会</div>
+                <div class="content">识途社</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sma.png"></div>
+            <div class="container">
+                <div class="content"></div>
+                <div class="content">三农协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/stiyu.png"></div>
+            <div class="container">
+                <div class="content">健身协会</div>
+                <div class="content">足球协会</div>
+                <div class="content">篮球协会</div>
+                <div class="content">武术协会</div>
+                <div class="content">乒乓球协会</div>
+                <div class="content">羽毛球协会</div>
+                <div class="content">体育舞蹈协会</div>
+                <div class="content">健美操协会</div>
+                <div class="content">跆拳道协会</div>
+                <div class="content">瑜伽协会</div>
+                <div class="content">排球社</div>
+                <div class="content">花样跳绳协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/stuanwei.png"></div>
+            <div class="container">
+                <div class="content">骆驼社</div>
+                <div class="content">吉他社</div>
+                <div class="content">辩论协会</div>
+                <div class="content">心理协会</div>
+                <div class="content">历史协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sbaowei.png"></div>
+            <div class="container">
+                <div class="content">无线电定向测向运动协会</div>
+                <div class="content">学生消防志愿者协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sxinxi.png"></div>
+            <div class="container">
+                <div class="content"></div>
+                <div class="content">教育信息化创新创业协会</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sjiuye.png"></div>
+            <div class="container">
+                <div class="content"></div>
+                <div class="content">支付宝俱乐部</div>
+            </div>
+        </div>
+        <div class="shetuan">
+            <div class="title"><img src="../../assets/sxuesheng.png"></div>
+            <div class="container">
+                <div class="content"></div>
+                <div class="content">创业俱乐部</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,8 +240,30 @@
         padding: 25px 5%;
         overflow-y: scroll;
     }
+    .shetuan {
+        width: 100%;
+        padding-bottom: 20px;
+        margin-bottom: 25px;
+        border-bottom: dotted 3px rgba(51, 11, 228, 0.3);
+        .title {
+            display: flex;
+            justify-content: center;
+        }
+        .container {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            padding-top: 25px;
+            .content {
+                width: 33.3%;
+                height: 25px;
+                text-align: center;
+                color: #432a92;
+            }
+        }
+    }
 
-    
+
     //
     ::-webkit-scrollbar {
         width: 30px;
